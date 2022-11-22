0df2e6fa6dbab24cebcb1bdf7d77298c084b4b2b
Wellington Cezario da Silva <116930080+WellingtonCezario@users.noreply.github.com>
2022-11-21 23:37:08 -0300

    asssssssss

---
+++ Exercicio025/cadastro.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+        Os dados foram enviados.
+    </h1>
+    <p>Aprenda PHP para saber lidar com eles!</p>
+    <form action="form002.html">
+        <input type="submit" value="Voltar.">
+    </form>
+</body>
+</html>
